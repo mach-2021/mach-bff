fb7b35af6c8d6396bbca5177619b8cdcd8d9583a
Vladyslav_Bieliaiev <Vladyslav_Bieliaiev@epam.com>
2021-01-24 14:06:22 +0200

    Update yaml deploy

--- bff/src/main/java/com/mach/bff/config/ApplicationConfiguration.java
+++ bff/src/main/java/com/mach/bff/config/ApplicationConfiguration.java
@@ -4,10 +4,12 @@ import com.mach.commercetools.config.CTConfiguration;
 import com.mach.core.config.CoreConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @Import({CTConfiguration.class,
         CoreConfiguration.class})
+@EnableSwagger2
 public class ApplicationConfiguration {
 
 }
